1ac435e112f5ef7345586b73aef46c92bc8c5ade
sndfsecurity <sndf4315@gmail.com>
2026-06-23 15:03:28 +0530

    url changes

--- services/authService.ts
+++ services/authService.ts
@@ -1,25 +1,25 @@
 import API_BASE_URL from "./api";
 
-export async function login(
-  email: string,
-  password: string
-) {
-  const response = await fetch(
-    `${API_BASE_URL}/auth/login`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email,
-        password,
-      }),
-    }
-  );
+// export async function login(
+//   email: string,
+//   password: string
+// ) {
+//   const response = await fetch(
+//     `${API_BASE_URL}/auth/login`,
+//     {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({
+//         email,
+//         password,
+//       }),
+//     }
+//   );
 
-  return response.json();
-}
+//   return response.json();
+// }
 
 export async function registerUser(
   name: string,
@@ -46,6 +46,36 @@ export async function registerUser(
   if (!response.ok) {
     throw new Error("User Registration Failed");
   }
+
+  return response.json();
+}
+
+
+
+console.log("API URL =", API_BASE_URL);
+
+export async function login(
+  email: string,
+  password: string
+) {
+
+  console.log("calling login");
+
+  const response = await fetch(
+    `${API_BASE_URL}/auth/login`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    }
+  );
+
+  console.log(response);
 
   return response.json();
 }
